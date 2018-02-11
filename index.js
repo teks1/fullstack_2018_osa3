310eb2bdc7e04446785e2ee5c27b3b2c1b2e5a79
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2018-02-11 19:48:03 +0200

    osa3_1

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const PORT = 3001
+
+app.use(bodyParser.json())
+
+let persons = [
+	{
+		id: 1,
+		name: 'Arto',
+		number: '666',
+	},
+	{
+		id: 2,
+		name: 'Arto 2',
+		number: '999',	
+	},
+    {
+		id: 3,
+		name: 'Martti',
+		number: '123',	
+	},
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id' ,(req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    let maara = persons.length
+    res.send(`luettelossa ${maara} henkilön tiedot <br>`  + new Date())
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (body.name === undefined || body.number === undefined) {
+        return res.status(400).json({error: 'name and/or number missing'})
+    } else if (persons.findIndex(p => p.name === body.name) > -1) {
+        return res.status(418).json({error: 'name taken'})
+    }
+    const person = {
+        id: Math.floor(Math.random() * Math.floor(1337)),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.listen(PORT)
